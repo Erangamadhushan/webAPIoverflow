ba368abbc55f3761e555933b0e8e3ffc5d1886e0
Erangamadhushan <erangamadhushann956@gmail.com>
2025-02-01 11:40:19 +0530

    Refactor renderPageContent function and update index.html layout with new header and icon

--- src/scripts/Home/index.js
+++ src/scripts/Home/index.js
@@ -1,25 +1,16 @@
-import {apicontent} from './content.js';
+//import {apicontent} from './content.js';
 
 window.addEventListener("DOMContentLoaded", () => {
-    renderPageContent(apicontent);
+    renderPageContent();
 });
 
-function renderPageContent(content) {
-    const root = document.querySelector(".root");
-    root.innerHTML = " ";
-    let rootContent = " ";
-    content.forEach((ele) => {
-        rootContent += `
-            <div class="my-5 min-h-100" >
-                <div class="card" style="min-height:300px;">
-                    <h2 class="text-purly text-center pb-2">${ele.title}</h2>
-                    <p>${ele.introduction.content}</p>
-                    <button type="buton" class="btn btn-purly my-3 text-gray" onclick="findContent()">Find Our More..</button>
-                </div>
-            </div>
-        `;
-    });
-
+function renderPageContent() {
+    // const root = document.querySelector(".mainContent");
+    // //root.innerHTML = " ";
+    // let rootContent = `
+        
+    // `;
+    
     root.innerHTML = rootContent;
 }
 
