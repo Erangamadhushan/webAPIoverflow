32cac87ab88494d55e67b8e6b2b8dbc127bb7d55
Erangamadhushan <erangamadhushann956@gmail.com>
2025-02-01 21:54:30 +0530

    Update index.html layout and enhance renderPageContent function to display API content dynamically

--- src/scripts/Home/index.js
+++ src/scripts/Home/index.js
@@ -1,17 +1,24 @@
-//import {apicontent} from './content.js';
-
+import {apicontent} from '../Data/content.js';
 window.addEventListener("DOMContentLoaded", () => {
-    renderPageContent();
+    renderPageContent(apicontent);
 });
 
-function renderPageContent() {
-    // const root = document.querySelector(".mainContent");
-    // //root.innerHTML = " ";
-    // let rootContent = `
-        
-    // `;
-    
-    root.innerHTML = rootContent;
+function renderPageContent(apicontents) {
+    const apiContentContainer = document.querySelector('.apiContentContainer');
+    apiContentContainer.innerHTML = '';
+    let apiContent = " ";
+    //console.log(apicontents);
+    apicontents.forEach((apicontent,$index) => {
+        console.log(apicontent.title);
+        apiContent += `
+            <div class="border border-amber-400 p-10 my-[20px]" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
+                <h2 class="text-white text-xl text-center py-5">${apicontent.title}</h2>
+                <p class="text-white text-lg pb-10">${apicontent.introduction.content}</p>
+                <button type="button" class="border border-amber-400 text-amber-400 p-3 rounded-xl cursor-pointer hover:bg-amber-400 hover:text-black" onclick="explorerAPIContent('${apicontent.title}')">Explorer More ..</button>
+            </div>
+        `
+    })
+    apiContentContainer.innerHTML = apiContent;
 }
 
 document.querySelector('form').addEventListener("submit", (event) => {
